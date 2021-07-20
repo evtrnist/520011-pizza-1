1d85f3dc1e3f134cf6a143d25a7c7c78a2c6b175
evtrnist <evagrandet@ro.ru>
2021-07-20 22:58:36 +0300

    Добавила компонент для подсчета ингредиентов

---
+++ src/frontend/src/common/components/ItemCounter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="counter counter--orange ingredients__counter">
+    <button
+      type="button"
+      class="counter__button counter__button--disabled counter__button--minus"
+    >
+      <span class="visually-hidden">Меньше</span>
+    </button>
+    <input type="text" name="counter" class="counter__input" :value="value" />
+    <button type="button" class="counter__button counter__button--plus">
+      <span class="visually-hidden">Больше</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemCounter",
+  props: {
+    value: {
+      type: Number,
+      require: true,
+    },
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
