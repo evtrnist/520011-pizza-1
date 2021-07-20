8ff1e7d4156e211e586babe07d29960b9e14b7aa
evtrnist <evagrandet@ro.ru>
2021-07-20 22:59:59 +0300

    Подключила добавленные компоненты

--- src/frontend/src/views/Index.vue
+++ src/frontend/src/views/Index.vue
@@ -4,119 +4,14 @@
       <div class="content__wrapper">
         <h1 class="title title--big">Конструктор пиццы</h1>
 
-        <div class="content__dough">
-          <div class="sheet">
-            <h2 class="title title--small sheet__title">Выберите тесто</h2>
+        <BuilderDoughSelector :doughs="doughs" />
 
-            <div class="sheet__content dough">
-              <label
-                :class="'dough__input dough__input--' + value"
-                v-for="{ id, name, description, value, isChecked } in doughs"
-                :key="id"
-              >
-                <input
-                  type="radio"
-                  name="dough"
-                  :value="value"
-                  class="visually-hidden"
-                  :checked="isChecked"
-                />
-                <b>{{ name }}</b>
-                <span>{{ description }}</span>
-              </label>
-            </div>
-          </div>
-        </div>
+        <BuilderSizeSelector :sizes="sizes" />
 
-        <div class="content__diameter">
-          <div class="sheet">
-            <h2 class="title title--small sheet__title">Выберите размер</h2>
-
-            <div class="sheet__content diameter">
-              <label
-                :class="'diameter__input diameter__input--' + value"
-                v-for="{ id, name, isChecked, value } in sizes"
-                :key="id"
-              >
-                <input
-                  type="radio"
-                  name="diameter"
-                  :value="value"
-                  class="visually-hidden"
-                  :checked="isChecked"
-                />
-                <span>{{ name }}</span>
-              </label>
-            </div>
-          </div>
-        </div>
-
-        <div class="content__ingredients">
-          <div class="sheet">
-            <h2 class="title title--small sheet__title">
-              Выберите ингредиенты
-            </h2>
-
-            <div class="sheet__content ingredients">
-              <div class="ingredients__sauce">
-                <p>Основной соус:</p>
-
-                <label
-                  class="radio ingredients__input"
-                  v-for="{ id, name, value, isChecked } in sauces"
-                  :key="id"
-                >
-                  <input
-                    type="radio"
-                    name="sauce"
-                    :value="value"
-                    :checked="isChecked"
-                  />
-                  <span>{{ name }}</span>
-                </label>
-              </div>
-
-              <div class="ingredients__filling">
-                <p>Начинка:</p>
-
-                <ul class="ingredients__list">
-                  <li
-                    class="ingredients__item"
-                    v-for="{ name, id, value, slug } in ingredients"
-                    :key="id"
-                  >
-                    <span :class="'filling filling--' + slug">{{ name }}</span>
-
-                    <div class="counter counter--orange ingredients__counter">
-                      <button
-                        type="button"
-                        class="
-                          counter__button
-                          counter__button--disabled
-                          counter__button--minus
-                        "
-                      >
-                        <span class="visually-hidden">Меньше</span>
-                      </button>
-                      <input
-                        type="text"
-                        name="counter"
-                        class="counter__input"
-                        :value="value"
-                      />
-                      <button
-                        type="button"
-                        class="counter__button counter__button--plus"
-                      >
-                        <span class="visually-hidden">Больше</span>
-                      </button>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        <BuilderIngredientsSelector
+          :ingredients="ingredients"
+          :sauces="sauces"
+        />
 
         <div class="content__pizza">
           <label class="input">
@@ -160,6 +55,10 @@ import normalizeSizes from "@/common/normalizeSizes";
 import normalizeSauces from "@/common/normalizeSauces";
 import normalizeIngredients from "@/common/normalizeIngredients";
 
+import BuilderDoughSelector from "@/modules/builder/components/BuilderDoughSelector";
+import BuilderSizeSelector from "@/modules/builder/components/BuilderSizeSelector";
+import BuilderIngredientsSelector from "@/modules/builder/components/BuilderIngredientsSelector";
+
 export default {
   name: "IndexLayout",
   data() {
@@ -173,6 +72,11 @@ export default {
     };
   },
   computed: {},
+  components: {
+    BuilderDoughSelector,
+    BuilderSizeSelector,
+    BuilderIngredientsSelector,
+  },
 };
 </script>
 
