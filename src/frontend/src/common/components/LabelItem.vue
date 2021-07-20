26a90b6b70285fc87a08fa54d28c341246f771e1
evtrnist <evagrandet@ro.ru>
2021-07-20 22:59:07 +0300

    Добавила абстрактный компонент label-input

---
+++ src/frontend/src/common/components/LabelItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <label :class="classes">
+    <input
+      type="radio"
+      :name="name"
+      :value="item.value"
+      class="visually-hidden"
+      :checked="item.isChecked"
+    />
+    <slot />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "LabelItem",
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+    classes: {
+      type: String,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
