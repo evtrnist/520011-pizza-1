2ed66a94ad9fbad24d5971a72950c92b2be7b5e0
evtrnist <evagrandet@ro.ru>
2021-07-20 22:59:29 +0300

    Добавила компонент для выбора опций теста

---
+++ src/frontend/src/modules/builder/components/BuilderDoughSelector.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="content__dough">
+    <div class="sheet">
+      <h2 class="title title--small sheet__title">Выберите тесто</h2>
+
+      <div class="sheet__content dough">
+        <LabelItem
+          v-for="item in doughs"
+          :key="item.id"
+          :item="item"
+          :classes="'dough__input dough__input--' + item.value"
+          :name="'dough'"
+        >
+          <b>{{ item.name }}</b>
+          <span>{{ item.description }}</span>
+        </LabelItem>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LabelItem from "@/common/components/LabelItem";
+export default {
+  name: "BuilderDoughSelector",
+  components: {
+    LabelItem,
+  },
+  props: {
+    doughs: {
+      type: Array,
+      require: true,
+    },
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
