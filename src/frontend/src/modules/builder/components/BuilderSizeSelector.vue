ba2774e1ccf874f7838aad7226ead648967d89b1
evtrnist <evagrandet@ro.ru>
2021-07-20 22:59:48 +0300

    Добавила компонент для выбора опций размера

---
+++ src/frontend/src/modules/builder/components/BuilderSizeSelector.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="content__diameter">
+    <div class="sheet">
+      <h2 class="title title--small sheet__title">Выберите размер</h2>
+
+      <div class="sheet__content diameter">
+        <LabelItem
+          v-for="item in sizes"
+          :key="item.id"
+          :item="item"
+          :classes="'diameter__input diameter__input--' + item.value"
+          :name="'diameter'"
+        >
+          <span>{{ item.name }}</span>
+        </LabelItem>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LabelItem from "@/common/components/LabelItem";
+
+export default {
+  name: "BuilderSizeSelector",
+  props: {
+    sizes: {
+      type: Array,
+      require: true,
+    },
+  },
+  components: {
+    LabelItem,
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
