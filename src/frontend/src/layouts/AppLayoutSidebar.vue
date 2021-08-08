f2364e76dacf67a6f540e77c0b0a6d8dbb112a69
evtrnist <evagrandet@ro.ru>
2021-08-08 11:53:28 +0300

    Добавила лайаут сайдбара

---
+++ src/frontend/src/layouts/AppLayoutSidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-layout-sidebar">
+    <AppLayoutHeader :is-auth="true" />
+    <main class="layout">
+      <div class="layout__sidebar sidebar">
+        <router-link to="/" class="logo layout__logo">
+          <img
+            src="@/assets/img/logo.svg"
+            alt="V!U!E! Pizza logo"
+            width="90"
+            height="40"
+          />
+        </router-link>
+
+        <router-link class="layout__link layout__link--active" to="/orders">
+          История заказов</router-link
+        >
+        <router-link class="layout__link" to="/profile">Мои данные</router-link>
+      </div>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader";
+
+export default {
+  name: "AppLayoutSidebar",
+  components: {
+    AppLayoutHeader,
+  },
+};
+</script>
